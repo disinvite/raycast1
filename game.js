f60f7a4a80f8ee4a93e1ea206eea3d63d1f1e328
disinvite <ms@disinvite.com>
2015-12-21 02:32:51 -0500

    click n drag righty

--- game.js
+++ game.js
@@ -1,14 +1,29 @@
 var el_canvas_game;
 var el_canvas_editor;
+var left_click_down = false;
+var right_click_down = false;
 
 function editorRightClick(event) {
     event.preventDefault();
-    console.log(event.pageX - el_canvas_editor.offsetLeft,event.pageY - el_canvas_editor.offsetTop);
+}
+
+function editorMouseChanged(event) {
+    left_click_down = (event.buttons & 1);
+    right_click_down = (event.buttons & 2);
+}
+
+function editorMouseMove(event) {
+    if(right_click_down) {
+        console.log(event.pageX - el_canvas_editor.offsetLeft,event.pageY - el_canvas_editor.offsetTop);
+    }
 }
 
 function setup() {
     el_canvas_editor = document.getElementById('map_editor');
     el_canvas_editor.addEventListener('contextmenu', editorRightClick, false);
+    el_canvas_editor.addEventListener('mousedown', editorMouseChanged, false);
+    el_canvas_editor.addEventListener('mousemove', editorMouseMove, false);
+    el_canvas_editor.addEventListener('mouseup', editorMouseChanged, false);
 }
 
 window.onload = function() {
