6ce8f724cf0f4a6e34fba65ce049b4f646b713ae
disinvite <ms@disinvite.com>
2015-12-21 02:06:19 -0500

    right click handler

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
+var el_canvas_game;
+var el_canvas_editor;
+
+function editorRightClick(event) {
+    event.preventDefault();
+    console.log(event.pageX - el_canvas_editor.offsetLeft,event.pageY - el_canvas_editor.offsetTop);
+}
+
+function setup() {
+    el_canvas_editor = document.getElementById('map_editor');
+    el_canvas_editor.addEventListener('contextmenu', editorRightClick, false);
+}
+
 window.onload = function() {
     console.log('hello');
+    setup();
 }
